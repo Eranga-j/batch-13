f7df4fcfbffe992b9bb9f273f6e762c38836635f
Gayan Eranga <168162990+Eranga-j@users.noreply.github.com>
2025-08-15 13:19:50 +0530

    Delete pahana-edu-service directory

--- pahana-edu-service/src/main/java/com/pahanaedu/service/ApplicationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.pahanaedu.service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ApplicationConfig extends Application { }
